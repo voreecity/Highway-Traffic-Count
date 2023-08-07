450792fca951b3565789832e9d5f682d665824a9
voreecity <praneet2407singh@gmail.com>
2023-08-07 13:11:11 +0530

    initial file upload

---
+++ main.py
@@ -0,0 +1,84 @@
+import numpy as np
+from ultralytics import YOLO
+import cv2
+import cvzone
+import math
+from sort import *
+
+cap = cv2.VideoCapture("../Videos/highway4.mp4")  # For Video
+
+model = YOLO("../Yolo-Weights/yolov8n.pt")
+
+classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+              "teddy bear", "hair drier", "toothbrush"]
+
+# tracking
+tracker = Sort(max_age=10, min_hits=3, iou_threshold=0.3)
+
+# line
+
+limits = [394, 471, 1162, 474]
+totalCount = []
+
+# mask
+mask = cv2.imread("mask4.png")
+
+while True:
+    success, img = cap.read()
+    maskedRegion = cv2.bitwise_and(mask, img)
+
+    results = model(maskedRegion, stream=True)
+    detections = np.empty((0, 5))
+
+    for r in results:
+        boxes = r.boxes
+        for box in boxes:
+            # Bounding Box
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+            w, h = x2 - x1, y2 - y1
+            # Confidence
+            conf = math.ceil((box.conf[0] * 100)) / 100
+            # Class Name
+            cls = int(box.cls[0])
+            currentClass = classNames[cls]
+            if (currentClass == "car" or currentClass == "motorbike" or currentClass == "bus" or
+                    currentClass == "truck" and conf > 0.5):
+                # cvzone.cornerRect(img, (x1, y1, w, h), l=10, rt=5)
+                # cvzone.putTextRect(img, f'{currentClass} {conf}', (max(0, x1), max(35, y1)),
+                #                   scale=1.5, thickness=2, offset=7)
+                currentArray = np.array([x1, y1, x2, y2, conf])
+                detections = np.vstack((detections, currentArray))
+
+    resultsTracker = tracker.update(detections)
+
+    cv2.line(img, (limits[0], limits[1]), (limits[2], limits[3]), (0, 0, 255), 5)
+
+    for result in resultsTracker:
+        x1, y1, x2, y2, id = result
+        x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+        w, h = x2 - x1, y2 - y1
+        cvzone.cornerRect(img, (x1, y1, w, h), l=10, rt=2, colorR=(255, 0, 0))
+        cvzone.putTextRect(img, f'{int(id)}', (max(0, x1), max(35, y1)),
+                           scale=1.5, thickness=2, offset=7)
+        print(result)
+        cx, cy = x1 + w // 2, y1 + h // 2
+        cv2.circle(img, (cx, cy), 5, (255,0,255), cv2.FILLED)
+
+        if limits[0] < cx < limits[2] and limits[1] - 10 < cy < limits[3] + 10:
+            if totalCount.count(id) == 0:
+                totalCount.append(id)
+                cv2.line(img, (limits[0], limits[1]), (limits[2], limits[3]), (0, 255, 0), 5)
+
+    cvzone.putTextRect(img, f' count: {len(totalCount)}', (50, 50))
+    cv2.imshow("Image", img)
+    # cv2.imshow("Masked region", maskedRegion)
+    cv2.waitKey(1)
